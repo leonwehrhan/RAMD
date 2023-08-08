c8a95a59ba651557c7674363f1edf9050dd8ccb7
Leon Wehrhan <mail@leonwehrhan.de>
2023-08-08 12:13:10 +0200

    read_residence_times

--- tRAMD.py
+++ tRAMD.py
@@ -1,3 +1,52 @@
 import numpy as np
+import re
 
 
+def read_residence_times(files, mode='log', timestep=2e-6):
+    '''
+    Read either .log files or .out file to gather the residence times from RAMD simulations
+
+    Parameters
+    ----------
+    files : str or list
+        Paths to .log/.out files. Must be list for .log, can be str or list with length 1 for .out.
+    mode : str
+        Whether to read .log or .out files. Either "log" or "out".
+    timestep : float
+        Simulation timestep in ns.
+    
+    Returns
+    -------
+    times : list
+        Residence times in ns.
+    '''
+    times = []
+
+    if mode == 'log':
+        for fi in files:
+            with open(fi, 'r') as f:
+                for line in f.readlines():
+                    if re.match(r'==== RAMD ==== GROMACS will be stopped', line):
+                        s = re.search(r'[0-9]+', line)[0]
+                        t = round(int(s) * timestep, 3)
+                        times.append(t)
+    
+    elif mode == 'out':
+        if files.isinstance(str):
+            fi = files
+        elif files.isinstance(list):
+            fi = files[0]
+            with open(fi, 'r') as f:
+                for line in f.readlines():
+                    if re.match(r'==== RAMD ==== GROMACS will be stopped', line):
+                        s = re.search(r'[0-9]+', line)[0]
+                        t = round(int(s) * timestep, 3)
+                        times.append(t)
+
+    else:
+        raise ValueError('mode must be "log" or "out".')
+
+    if files.isinstance(list):
+        print(f'Found {len(times)} residence times in {len(files)} files.')
+    else:
+        print(f'Found {len(times)} residence times in 1 file.')
