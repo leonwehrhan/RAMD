d3197e8d14519270c0c3a3c1aa2d33924cacf864
Leon Wehrhan <mail@leonwehrhan.de>
2023-08-14 16:28:32 +0200

    bugfix

--- RAMD_gmx.sh
+++ RAMD_gmx.sh
@@ -14,9 +14,9 @@ module add GROMACS/2020.5-RAMD-2.0-fosscuda-2019b;
 mkdir sim${SLURM_ARRAY_TASK_ID};
 cd sim${SLURM_ARRAY_TASK_ID};
 
-sed "s/XX/${i}/"  ../MDP/md.mdp > md${SLURM_ARRAY_TASK_ID}.mdp;
+sed "s/XX/${SLURM_ARRAY_TASK_ID}/"  ../MDP/md.mdp > md${SLURM_ARRAY_TASK_ID}.mdp;
 
-gmx grompp -f ../MDP/md${SLURM_ARRAY_TASK_ID}.mdp -c ../umb0_start.gro -p ../topol.top -n ../index.ndx -o md${SLURM_ARRAY_TASK_ID}.tpr;
+gmx grompp -f md${SLURM_ARRAY_TASK_ID}.mdp -c ../umb0_start.gro -p ../topol.top -n ../index.ndx -o md${SLURM_ARRAY_TASK_ID}.tpr;
 
 gmx mdrun -deffnm md${SLURM_ARRAY_TASK_ID};
 echo 18 0 | gmx trjconv -s md${SLURM_ARRAY_TASK_ID}.tpr -f md${SLURM_ARRAY_TASK_ID}.xtc -n ../index.ndx -center -pbc mol -o md${SLURM_ARRAY_TASK_ID}_noPBC.xtc;
